e4af0a24c27725b238d14a1067adcee61787dea9
Lev <mynameislev@mail.ru>
2021-02-04 14:19:21 +0700

    разделил form&elements на отдельные блоки миксинами, теперь делаю cards

--- webpack.config.js
+++ webpack.config.js
@@ -120,7 +120,7 @@ module.exports = {
                         loader: "file-loader",
                         options: {
                           name: 'img/[name].[ext]'
-                        },
+                        }
                       }
                 ]
             },
